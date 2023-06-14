d907311b4c6a98627c33155bd55b070b041e9915
Dhruv Sabharwal <dhruvsabds@gmail.com>
2023-06-14 02:10:22 -0400

    changed 404 meassage

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/nfts", nfts);
 app.use("*", (req, res) => {
-  res.status(404).json({ error: "not found" });
+  res.status(404).json({ error: "not found, use route /api/v1/nfts instead" });
 });
 
 export default app;
